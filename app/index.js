b1ecff4d61a2470c606cc819ebee6210e9c87a76
mario <mario.kristovit@dxtr.asia>
2024-01-22 17:22:06 +0700

    feat : update redux toolkit package

--- app/index.js
+++ app/index.js
@@ -1,35 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from 'react-redux';
 import "../global.css"
+import { store } from "./redux/store";
+import Counted from "./components/counted";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text className="text-blue-500">Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <Provider store={store}>
+        <Counted />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
