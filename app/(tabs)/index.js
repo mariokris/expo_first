e8e8f245fbf5cba5f8b36226b3fab57817ccb564
mario <mario.kristovit@dxtr.asia>
2024-01-30 20:42:53 +0700

    feat : add new auth reducer

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,12 +1,15 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { Provider } from 'react-redux';
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { Provider, useSelector, useDispatch } from 'react-redux';
 import { store } from "../redux/store";
 import { Link, Stack, router } from "expo-router";
+import { setToken } from "../redux/authSlice";
 
 export default function Page() {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state.auth);
+  console.log('state : ', state);
   return (
     <View>
-      <Text>User </Text>
       <Link href={"users/1"}>Go to user 1</Link>
       <Pressable onPress={() => router.push({
         pathname: "users/[id]",
@@ -14,6 +17,8 @@ export default function Page() {
       })}>
         <Text>Go to user 2</Text>
       </Pressable>
+      <Text className="text-3xl font-bold">Token: {state.token}</Text>
+      <Button title="ubah token" onPress={() => dispatch(setToken('n12312h1kj34hj3HJDFHhjhj'))}/>
     </View>
   );
 }
