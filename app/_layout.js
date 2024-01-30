0ca9a83af2b13eb8a9ee218c3e2d933d802cc624
mario <mario.kristovit@dxtr.asia>
2024-01-30 20:42:38 +0700

    feat : add new native wind config

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import "../global.css"
 
 // export default function RootLayout() {
 //   return (
