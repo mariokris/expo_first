a031553e3599b1c14dd62f849183819805be390c
mario <mario.kristovit@dxtr.asia>
2024-02-01 12:04:18 +0700

    fix : linter

--- app/(tabs)/users/counted.js
+++ app/(tabs)/users/counted.js
@@ -1,9 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { increment, decrement, incrementByAmount } from '../../redux/counterReducer';
+import {StatusBar} from 'expo-status-bar';
+import {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from '../../redux/counterReducer';
 
 export default function Counted() {
   const dispatch = useDispatch();
@@ -11,18 +15,23 @@ export default function Counted() {
   const [count, setCount] = useState(0);
   const incrementJupiter = () => {
     setCount(count + 1);
-  }
+  };
   const decrementJupiter = () => {
     setCount(count - 1);
-  }
+  };
   return (
-        <View className='flex-1 items-center justify-center bg-yellow-100'>
-      <View className='p-4 border-2 border-lightblue-500 rounded'>
-        <Text className='text-3xl font-bold'>Hello Jupiter {counterState.value}</Text>
+    <View className="flex-1 items-center justify-center bg-yellow-100">
+      <View className="p-4 border-2 border-lightblue-500 rounded">
+        <Text className="text-3xl font-bold">
+          Hello Jupiter {counterState.value}
+        </Text>
       </View>
-      <Button title="Increment Jupiter" onPress={() => dispatch(increment())}/>
-      <Button title="Decrement Jupiter" onPress={() => dispatch(decrement())}/>
-      <Button title="Increment Jupiter Number" onPress={() => dispatch(incrementByAmount(90))}/>
+      <Button title="Increment Jupiter" onPress={() => dispatch(increment())} />
+      <Button title="Decrement Jupiter" onPress={() => dispatch(decrement())} />
+      <Button
+        title="Increment Jupiter Number"
+        onPress={() => dispatch(incrementByAmount(90))}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- app/redux/authSlice.js
+++ app/redux/authSlice.js
@@ -1,17 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-    token: '',
+  token: '',
 };
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setToken(state, action){
-            state.token = action.payload;
-        },
-    }
+  name: 'auth',
+  initialState,
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+  },
 });
 
 export const {setToken} = authSlice.actions;
