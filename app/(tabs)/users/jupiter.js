fbc2e75381603e6bfa80fa55821f5d74814cc38b
mario <mario.kristovit@dxtr.asia>
2024-02-01 15:22:03 +0700

    ngasal

--- app/(tabs)/users/jupiter.js
+++ app/(tabs)/users/jupiter.js
@@ -4,7 +4,7 @@ import { TouchableOpacity, View, Text } from 'react-native';
 export default function User() {
    const goToJupiter = () => {
         router.replace('../pages/counted');
-    }
+    };
 
     return (
         <View className='flex-1 items-center justify-center bg-yellow-100'>
